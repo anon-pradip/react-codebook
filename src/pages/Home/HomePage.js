c1f40583394c422a67f525065eb4355b9c600f26
anon-pradip <pradipsubedi360@gmail.com>
2023-05-04 14:21:03 +0545

    make static home page

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Hero from './components/Hero'
+import FeaturedProducts from './components/FeaturedProducts'
+import Testimonials from './components/Testimonials'
+import Faq from './components/Faq'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='min-h-screen mt-5'>
+      <Hero />
+      <FeaturedProducts />
+      <Testimonials />
+      <Faq />
+    </div>
   )
 }
 
--- src/pages/Home/components/Hero.js
+++ src/pages/Home/components/Hero.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import HeroImage from "../../../assets/image/hero.avif"
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className=' flex flex-col md:flex-row-reverse justify-center md:justify-between items-center max-w-5xl mx-auto '>
+      <div className='max-w-lg mx-auto px-2'>
+        <img src={HeroImage} alt="hero-section" className=' rounded-md' />
+      </div>
+      <div className='md:mt-9 text-center md:text-start'>
+        <h1 className=' font-bold text-4xl py-4'>The Ultimate eBook Store</h1>
+        <p className=' text-black pb-6'>CodeBook is the World's most popular and authoritative source for computer science ebooks. Find rating and access to the newest books digitally.</p>
+
+        <Link to="/products" className=' px-4 py-2 bg-blue-800 text-white rounded-md'>
+          Explore Books
+        </Link>
+      </div>
+    </div>
   )
 }
 
