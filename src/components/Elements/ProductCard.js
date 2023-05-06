b186273269484747f1bc6f0471793a6c2fb3fbeb
anon-pradip <pradipsubedi360@gmail.com>
2023-05-06 08:11:53 +0545

    use json server for featured products

--- src/components/Elements/ProductCard.js
+++ src/components/Elements/ProductCard.js
@@ -1,28 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { StarIcon } from "@heroicons/react/24/outline"
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+  const { id, name, overview, long_description, price, poster, image_local, rating } = product
+
   return (
     <div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
       <Link to="" className="relative" >
         {/* { best_seller && <span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">Best Seller</span> } */}
-        <img className="rounded-t-lg w-full h-64" src="/assets/images/10001.avif" alt='test-for-now' />
+        <img className="rounded-t-lg w-full h-64" src={poster} alt='test-for-now' />
       </Link>
       <div className="p-5">
         <Link to="">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ProductCard</h5>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
         </Link>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Description</p>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
 
         <div className="flex items-center my-2">
           {/* <Rating rating={rating} /> */}
-          Ratings
+          <StarIcon height={20} width={20} className=' text-white fill-yellow-600' />
+          <StarIcon height={20} width={20} className=' text-white fill-yellow-600' />
+          <StarIcon height={20} width={20} className=' text-white fill-yellow-600' />
+          <StarIcon height={20} width={20} className=' text-white fill-yellow-600' />
         </div>
 
         <p className="flex justify-between items-center">
           <span className="text-2xl dark:text-gray-200">
             {/* <span>$</span><span>{price}</span> */}
-            <span>$</span><span>Price</span>
+            <span>$</span><span>{price}</span>
 
           </span>
           {/* { !inCart && <button onClick={() => addToCart(product)} className={`inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 ${product.in_stock ? "" : "cursor-not-allowed"}`} disabled={ product.in_stock ? "" : "disabled" }>Add To Cart <i className="ml-1 bi bi-plus-lg"></i></button> }  
