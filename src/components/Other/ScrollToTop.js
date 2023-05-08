5107e16b8c2ec32bb12302d6c378ed68792c27a1
anon-pradip <pradipsubedi360@gmail.com>
2023-05-08 12:21:50 +0545

    fix scroll to top

--- src/components/Other/ScrollToTop.js
+++ src/components/Other/ScrollToTop.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const ScrollToTop = () => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
   return (
-    <div>ScrollToTop</div>
+    null
   )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Header from "./Layouts/Header"
 import Footer from "./Layouts/Footer"
 import ProductCard from "./Elements/ProductCard"
 import Rating from "./Elements/Rating"
+import ScrollToTop from "./Other/ScrollToTop"
 
-export { Header, Rating, Footer, ProductCard }
+export { Header, Rating, Footer, ProductCard, ScrollToTop }
